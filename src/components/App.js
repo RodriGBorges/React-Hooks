a67c24cddd8fa6aab3d4ef480da422afb9cd7bce
RodriGBorges <gabii.borges1998@gmail.com>
2022-01-22 23:39:33 -0300

    Base del TP

---
+++ src/components/App.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import SideBar from './SideBar';
+import Footer from './Footer';
+import TopBar from './TopBar';
+import ContentRowMovies from './ContentRowMovies';
+import Chart from './Chart';
+import LastMovieInDb from './LastMovieInDb';
+import GenresInDB from './GenresInDb'
+import NotFound from './NotFound';
+import { Routes, Route } from 'react-router-dom';
+
+/* import GenresInDb from './GenresInDb' */
+
+function App() {
+  return (
+    <React.Fragment>
+      	<div id="wrapper">
+          <SideBar />
+          <div className='content'>
+            <TopBar />
+            <ContentRowMovies />
+            {/* Inicio de campo de muestra */}
+            <div className='center'>
+              <Routes>
+                <Route path='/lastmovie' element={<LastMovieInDb/>}/>
+                <Route path='/genres' element={<GenresInDB/>}/>
+                <Route path='/content' element={<Chart/>}/>
+                <Route path='*' element={<NotFound/>}/>
+              </Routes>
+            </div>
+            {/* Fin de campo de muestra */}
+            <Footer />
+          </div>
+        </div>
+    </React.Fragment>
+  );
+}
+
+export default App;
